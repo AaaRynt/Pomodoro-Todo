be315cca4df545d5bc25d76ec2de09fb5691d15b
RynT <122379612+AaaRynt@users.noreply.github.com>
2025-12-21 20:59:29 +0800

    router-fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,11 @@
+// https://analytics.google.com/analytics/web/
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+	dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "G-DBVDLF0RZ6");
+
 import { createRouter, createWebHistory } from "vue-router";
 import Chart from "@/pages/chart.vue";
 import Todos from "@/pages/todos.vue";
@@ -5,7 +13,7 @@ import Setting from "@/pages/setting.vue";
 const router = createRouter({
 	history: createWebHistory(),
 	routes: [
-		{ path: "/", redirect: "/chart" },
+		{ path: "/", redirect: "/todos" },
 		{ path: "/chart", component: Chart },
 		{ path: "/todos", component: Todos },
 		{ path: "/setting", component: Setting },
